ad2d7f8d7311293826c9cae1ca3ea383edacfc1f
Shinokama <34408095+Shinokama@users.noreply.github.com>
2021-07-19 16:20:44 +0300

    scripts have been added

---
+++ task-1.py
@@ -0,0 +1,21 @@
+# string variable
+a = "Hello World"
+print(a)
+
+# int variable
+a = 1
+print(a)
+
+# float variable
+a = 1.5
+print(a)
+
+# ask for user input (string)
+a = input("Введите любое слово: ")
+b = input("Введите еще одно слово: ")
+print(f'Спасибо! Вот ваши слова: "{a}" и "{b}"')
+
+# ask for user input (int)
+a = int(input("Введите любое целое число: "))
+b = int(input("Введите еще одно целое число: "))
+print(f'Спасибо! Вот ваши числа: "{a}" и "{b}", их сумма равна: {a+b}')
